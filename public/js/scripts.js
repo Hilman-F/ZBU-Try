b349eb230dc1da44a5d6c8baba9782ce9b131a49
Hilman-F <muhammadhilmanfairuzi@gmail.com>
2025-07-26 01:23:09 +0700

    Update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -41,13 +41,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const promoTitle = document.getElementById("promoTitle");
 
   const promoData = {
-    best: ["../../assets/paket/fix2-nb-01.png"],
-    hemat: ["../../assets/paket/fix2-nb-01.png"],
-    promo: [],
-    berkah: ["../../assets/paket/fix2-nb-01.png"],
-    vip: ["../../assets/paket/fix2-nb-01.png"],
-    vvip: ["../../assets/paket/fix2-nb-01.png"],
-    all: ["../../assets/paket/fix2-nb-01.png"],
+    best: [
+      "../../assets/paket/Best/b1.jpg",
+      "../../assets/paket/Best/b2.jpg",
+      "../../assets/paket/Best/b3.jpg",
+      "../../assets/paket/Best/b4.jpg",
+      "../../assets/paket/Best/b5.jpg",
+      "../../assets/paket/Best/b6.jpg",
+      "../../assets/paket/Best/b7.jpg",
+      "../../assets/paket/Best/b8.jpg",
+      "../../assets/paket/Best/b9.jpg",
+      "../../assets/paket/Best/b10.jpg",
+      "../../assets/paket/Best/b11.jpg",
+      "../../assets/paket/Best/b12.jpg",
+    ],
+    berkah: ["../../assets/paket/Berkah/b1-01.jpg", "../../assets/paket/Berkah/b2-01.jpg", "../../assets/paket/Berkah/b3-01.jpg"],
+    hemat: ["../../assets/paket/Hemat/h1-01.jpg"],
+    promo: ["../../assets/paket/Promo/p1-01.jpg", "../../assets/paket/Promo/p2-01.jpg"],
+    vip: ["../../assets/paket/VIP/v1-01.jpg"],
+    vvip: ["../../assets/paket/VVIP/vvip-01.jpg"],
+    special: ["../../assets/paket/Special/s1-01.jpg"],
+    all: [],
   };
 
   function getCategoryFromPath() {
@@ -93,44 +107,53 @@ document.addEventListener("DOMContentLoaded", function () {
         img.className = "rounded  h-auto z-0";
 
         img.setAttribute("data-aos", "zoom-out");
-        img.setAttribute("data-aos-delay", `${index * 300}`);
+        img.setAttribute("data-aos-delay", `${index * 200}`);
         // promoContainer.appendChild(wrapper);
         // wrapper.appendChild(img);
 
         // Tombol Daftar Sekarang
         const daftarBtn = document.createElement("a");
-        daftarBtn.href = "#daftar";
-        daftarBtn.textContent = "Daftar";
+        daftarBtn.href = "https://wa.me/6282143551888";
+        daftarBtn.textContent = "Daftar Sekarang";
         daftarBtn.className =
-          "absolute top-[86%] left-[35%] -translate-x-1/2 -translate-y-1/2 " +
-          "bg-white text-[#447EBE] text-[0.25rem] font-bold px-1 py-0.5 rounded-full shadow-lg sm:text-[0.5rem] md:text-xs lg:text-base lg:px-2 lg:py-1 2xl:text-xl " +
-          "hover:bg-blue-100 transition z-10";
+          "absolute top-[86%] left-[42%] -translate-x-1/2 -translate-y-1/2 " +
+          "bg-white text-[#447EBE] text-[0.25rem] font-bold px-3 py-0.5 rounded-full shadow-lg sm:text-[0.5rem] md:text-xs lg:text-base 2xl:text-xl " +
+          "hover:bg-[#447EBE] hover:text-white transition z-10";
 
-        // Tombol Hubungi Kami
-        const hubungiBtn = document.createElement("a");
-        hubungiBtn.href = "https://wa.me/6282143551888";
-        hubungiBtn.className =
-          "absolute top-[86%] left-[45%] -translate-x-1/2 -translate-y-1/2 lg:left-[46%] " +
-          "bg-[#447EBE] text-white text-[0.25rem] font-bold px-1 py-0.5 rounded-full shadow-lg sm:text-[0.5rem] md:text-xs lg:text-base lg:px-2 lg:py-1 2xl:text-xl " +
-          "hover:bg-blue-100 transition z-10 flex items-center space-x-1";
-        
-        const spanText = document.createElement("span");
-        spanText.textContent = "WhatsApp";
+        // // Tombol Daftar Sekarang
+        // const daftarBtn = document.createElement("a");
+        // daftarBtn.href = "#daftar";
+        // daftarBtn.textContent = "Daftar";
+        // daftarBtn.className =
+        //   "absolute top-[86%] left-[35%] -translate-x-1/2 -translate-y-1/2 " +
+        //   "bg-white text-[#447EBE] text-[0.25rem] font-bold px-1 py-0.5 rounded-full shadow-lg sm:text-[0.5rem] md:text-xs lg:text-base lg:px-2 lg:py-1 2xl:text-xl " +
+        //   "hover:bg-blue-100 transition z-10";
 
-        // Icon Whatsapp
-        const whatsappIcon = document.createElement("img");
-        whatsappIcon.src = "../../assets/Icon/WhatsApp-w.png";
-        whatsappIcon.alt = "Icon Whatsapp";
-        whatsappIcon.className = "w-1.5 h-1.5 inline-block ml-0.5 sm:w-2 sm:h-2 md:w-3 md:h-3 lg:w-4 lg:h-4 2xl:w-5 2xl:h-5";
-        hubungiBtn.appendChild(whatsappIcon);
-        hubungiBtn.appendChild(spanText);
+        // // Tombol Hubungi Kami
+        // const hubungiBtn = document.createElement("a");
+        // hubungiBtn.href = "https://wa.me/6282143551888";
+        // hubungiBtn.className =
+        //   "absolute top-[86%] left-[45%] -translate-x-1/2 -translate-y-1/2 lg:left-[46%] " +
+        //   "bg-[#447EBE] text-white text-[0.25rem] font-bold px-1 py-0.5 rounded-full shadow-lg sm:text-[0.5rem] md:text-xs lg:text-base lg:px-2 lg:py-1 2xl:text-xl " +
+        //   "hover:bg-blue-100 transition z-10 flex items-center space-x-1";
+
+        // const spanText = document.createElement("span");
+        // spanText.textContent = "WhatsApp";
+
+        // // Icon Whatsapp
+        // const whatsappIcon = document.createElement("img");
+        // whatsappIcon.src = "../../assets/Icon/WhatsApp-w.png";
+        // whatsappIcon.alt = "Icon Whatsapp";
+        // whatsappIcon.className = "w-1.5 h-1.5 inline-block ml-0.5 sm:w-2 sm:h-2 md:w-3 md:h-3 lg:w-4 lg:h-4 2xl:w-5 2xl:h-5";
+        // hubungiBtn.appendChild(whatsappIcon);
+        // hubungiBtn.appendChild(spanText);
 
         // wrapper.appendChild(hubungiBtn);
-        
+
         promoContainer.appendChild(wrapper);
         wrapper.appendChild(img);
         wrapper.appendChild(daftarBtn);
-        wrapper.appendChild(hubungiBtn);
+        // wrapper.appendChild(hubungiBtn);
         // baru masukkan wrapper ke halaman
       });
     }

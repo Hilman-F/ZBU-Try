520feee4205b9a9250132a5742c24cc7aaf6a7db
Hilman-F <muhammadhilmanfairuzi@gmail.com>
2025-08-20 00:46:01 +0700

    DONE

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -122,7 +122,7 @@ document.addEventListener("DOMContentLoaded", function () {
         daftarBtn.href = "https://wa.me/6282143551888";
         daftarBtn.textContent = "Daftar Sekarang";
         daftarBtn.className =
-          "absolute top-[90%] left-[48%] -translate-x-1/2 -translate-y-1/2 " +
+          "absolute top-[86%] left-[42%] -translate-x-1/2 -translate-y-1/2 " +
           "bg-white text-[#447EBE] text-[0.25rem] font-bold px-3 py-0.5 rounded-full shadow-lg sm:text-[0.5rem] md:text-xs lg:text-base 2xl:text-xl " +
           "hover:bg-[#447EBE] hover:text-white transition z-10";
 
